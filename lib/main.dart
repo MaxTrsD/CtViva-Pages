4188d699db3ed07ded9070adfe47983a95824b3e
MaxTrsD <maxneydomingos@hotmail.com>
2024-09-12 09:40:12 -0300

    Deploy Web App

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Web Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Web App'),
+      ),
+      body: Center(
+        child: Text('Olá, bem-vindo ao Flutter para Web!'),
+      ),
+    );
+  }
+}
